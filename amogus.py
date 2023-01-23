4ac638caf0b386bc8e0d3140729dcd537fff7802
Hiatus <77402029+hiatus770@users.noreply.github.com>
2023-01-23 22:08:09 +0000

    Pending changes exported from your codespace

--- amogus.py
+++ amogus.py
@@ -1,1 +1,16 @@
+file = open("digits.txt", "r")
+digits = file.read()
 
+def patternSet(index):
+    cur = []
+    for i in range(16):
+        cur.append(digits[index + i])
+
+binary = ""
+for digit in digits:
+    binary += bin(int(digit))[2:]
+
+pattern = [0,1,1,1,1,1,0,0,1,1,1,1,0,1,0,1]
+
+for i in range(): 
+    
