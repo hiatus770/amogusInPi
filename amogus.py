3e7904bef491ec92e8d829bdeeaf52cdda7a7253
hiatus <matiasrivas3106@gmail.com>
2023-01-23 22:08:57 -0500

    found the amogus!!!

--- amogus.py
+++ amogus.py
@@ -1,16 +1,29 @@
 file = open("digits.txt", "r")
 digits = file.read()
 
-def patternSet(index):
-    cur = []
-    for i in range(16):
-        cur.append(digits[index + i])
-
 binary = ""
 for digit in digits:
     binary += bin(int(digit))[2:]
 
-pattern = [0,1,1,1,1,1,0,0,1,1,1,1,0,1,0,1]
+def patternSet(index):
+    cur = ""
+    for i in range(16):
+        cur += (binary[index + i])
+    return cur
 
-for i in range(): 
-    
+def printPattern(pattern):
+    for i in range(4):
+        for j in range(4):
+            if (pattern[(i*4)+j] == "0"):
+                print(" ", end="")
+            if (pattern[(i*4)+j] == "1"):
+                print("#", end="")
+        print()
+
+pattern = "1111100110011111" # "1111100110011111" # "0111110011110101" # Amogus pattern
+
+for i in range(len(digits)-15):
+    if (pattern == binary[i:i+16]):
+        print("Pattern matched at", i)
+        printPattern(pattern)
+        
